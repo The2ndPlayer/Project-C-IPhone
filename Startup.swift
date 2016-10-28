3b39efc193c7822acb7e75c67e72a4b0605f4ad0
Jackson Tweet <jacksontweet@createninnovate.com>
2016-10-28 14:34:49 -0500

    Updating Dev Version of the start up file
    im bad at this

--- Startup.swift
+++ Startup.swift
@@ -4,9 +4,15 @@
 ||-----Beta Testers:(I will put a link here later)-----||
  \*****************************************************/
 
+var Welcome = "Welcome to the Developement version of Project 'c'";
+var m2 = "If you are here arent a teser and you managed to compile and start this app!";
+var m3 = "good job, i honestly applod you due to me not even knowing how to compile Swift.";
+var m4 = "Especially, Since i had a compiler on my USB but my friend unpluged it in the middle of a compile and broke the USB and the file system."
 
-var Welcome = "Welcome to the Developement version of Project 'c'"
-
-
-
-print(welcome)
+println(welcome)
+println()
+println(m2)
+println()
+println(m3)
+println()
+println(m4)

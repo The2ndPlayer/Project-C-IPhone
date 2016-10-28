e96a5d7ba638d6c59619c9d5efeed8103fbe0d2b
Jackson Tweet <jacksontweet@createninnovate.com>
2016-10-28 14:20:42 -0500

    Updating StartUp file
    Added: Credit for beta testers and Developers

--- Startup.swift
+++ Startup.swift
@@ -1,1 +1,12 @@
+ /*****************************************************\
+||-------Project 'C' Is Owned by: The 2nd Player-------||
+||---Lead Developer & Project Manager: Jackson Tweet---||
+||-----Beta Testers:(I will put a link here later)-----||
+ \*****************************************************/
 
+
+var Welcome = "Welcome to the Developement version of Project 'c'"
+
+
+
+print(welcome)

a4a0309d98e887e55d275abb1a707fcfa23a4cce
Jackson Tweet <jacksontweet@createninnovate.com>
2016-10-28 14:21:18 -0500

    Update Startup.swift

--- Startup.swift
+++ Startup.swift
@@ -1,1 +1,7 @@
+ /*****************************************************\
+||-------Project 'C' Is Owned by: The 2nd Player-------||
+||---Lead Developer & Project Manager: Jackson Tweet---||
+||-----Beta Testers:(I will put a link here later)-----||
+ \*****************************************************/
+
 var Welcome ="welcome"

6def130e586ceb69f51a4d6b34fa73b19c0d2cf6
Jackson Tweet <jacksontweet@createninnovate.com>
2016-10-31 10:33:26 -0500

    Update Startup.swift

--- Startup.swift
+++ Startup.swift
@@ -4,15 +4,15 @@
 ||-----Beta Testers:(I will put a link here later)-----||
  \*****************************************************/
 
-var Welcome = "Welcome to the Developement version of Project 'c'";
-var m2 = "If you are here arent a teser and you managed to compile and start this app!";
-var m3 = "good job, i honestly applod you due to me not even knowing how to compile Swift.";
-var m4 = "Especially, Since i had a compiler on my USB but my friend unpluged it in the middle of a compile and broke the USB and the file system."
+let welcome = "Welcome to the Developement version of Project c";
+let m2 = "If you are here arent a teser and you managed to compile and start this app!";
+let m3 = "good job, i honestly applod you due to me not even knowing how to compile Swift.";
+let m4 = "Especially, Since i had a compiler on my USB but my friend unpluged it in the middle of a compile and broke the USB and the file.";
 
-println(welcome)
-println()
-println(m2)
-println()
-println(m3)
-println()
-println(m4)
+print("" + welcome)
+print("")
+print("" + m2)
+print("")
+print("" + m3)
+print("")
+print("" + m4)

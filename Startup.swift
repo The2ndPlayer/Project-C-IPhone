5800568a4594192be2d5bdb63c07cd658f52ca5d
Jackson Tweet <jacksontweet@createninnovate.com>
2016-11-02 10:41:48 -0500

    Update Startup.swift

--- Startup.swift
+++ Startup.swift
@@ -4,15 +4,13 @@
 ||-----Beta Testers:(I will put a link here later)-----||
  \*****************************************************/
 
-let welcome = "Welcome to the Developement version of Project c";
-let m2 = "If you are here arent a teser and you managed to compile and start this app!";
-let m3 = "good job, i honestly applod you due to me not even knowing how to compile Swift.";
-let m4 = "Especially, Since i had a compiler on my USB but my friend unpluged it in the middle of a compile and broke the USB and the file.";
+var Dev:Int = 1;
+var BuildVersion:Int = 1;
 
-print("" + welcome)
-print("")
-print("" + m2)
-print("")
-print("" + m3)
-print("")
-print("" + m4)
+if (Dev == 1) {
+	Print("NOTE: This is a development version of Project C!!")
+	Print("Build Number: "+ BuildVersion)
+} Else {
+ Print("")
+}
+
